f561750508d5e0ab8a471be4957547f4bc19fea5
Andro Manukov <manukov2@linux-05.ews.illinois.edu>
2019-09-05 01:44:56 -0500

    Part 2 progress

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -1,25 +1,41 @@
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
-
 #include <string>
+#include <ctime>
 
+using namespace cs225;
 
 void rotate(std::string inputFile, std::string outputFile)
 {
-  for (unsigned x = 0; x < inputFile.width(); x++)
+  PNG image1;
+  image1.readFromFile(inputFile);
+  PNG image2(image1.width(), image1.height());
+  for(unsigned int x = 0; x < image1.width(); x++)
   {
-    for (unsigned y = 0; y < inputFile.height(); y++)
+    for(unsigned int y = 0; y < image1.height(); y++)
     {
-      outputFile(x,y) = inputFile(inputFile.width() - x, inputFile.height() - y);
+      HSLAPixel & image1pixel = image1.getPixel(x, y);
+      HSLAPixel & image2pixel = image2.getPixel(image1.width() - 1 - x, image1.width() - 1 - y);
+      image2pixel = image1pixel;
     }
   }
 
-  return firstImage;
+  image2.writeToFile(outputFile);
+
 }
 
-cs225::PNG myArt(unsigned int width, unsigned int height) {
+cs225::PNG myArt(unsigned int width, unsigned int height)
+{
+  int i = 1;
   cs225::PNG png(width, height);
-  // TODO: Part 3
+  for(unsigned int x = 0; x < png.width() / 5; x++)
+  {
+    for(unsigned int y = 0; y < png.height(); y++)
+    {
+
+    }
+  }
+
 
   return png;
 }
