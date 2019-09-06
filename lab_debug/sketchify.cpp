e5a6db68f9079f03f3b8d7afbf474fa9ce78f02e
Andro Manukov <manukov2@linux-05.ews.illinois.edu>
2019-09-05 23:00:40 -0500

    Fixed all segfaults

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -29,8 +29,8 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(94, 0.8, 0.5);
-    return &p;
+    HSLAPixel* p = new HSLAPixel(97, 0.8, 0.5);
+    return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
@@ -40,15 +40,14 @@ void sketchify(std::string inputFile, std::string outputFile) {
     unsigned width = original->width(); //this bad
     unsigned height = original->height(); //this bad
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor();
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++) {
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -57,12 +56,10 @@ void sketchify(std::string inputFile, std::string outputFile) {
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
             HSLAPixel& currOutPixel = output->getPixel(x, y); //seg fault here what does output* do
-            std::cout << "Reached line " << __LINE__ << std::endl;
-            if (diff > 20)  {
-                currOutPixel = *myPixel; //segfault here
-            std::cout << "Reached line " << __LINE__ << std::endl;
+            if (diff > 20)
+            {
+                currOutPixel = *myPixel;
             }
-            std::cout << "Reached line " << __LINE__ << std::endl;
         }
     }
 
