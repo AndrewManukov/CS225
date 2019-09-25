55e4d2e144266d8e67bda8b75056ff7f1315a3d9
Andro Manukov <manukov2@linux-14.ews.illinois.edu>
2019-09-24 22:45:03 -0500

    created meme

--- mp_stickers/main.cpp
+++ mp_stickers/main.cpp
@@ -1,7 +1,29 @@
 #include "Image.h"
 #include "StickerSheet.h"
 
-int main() {
+int main()
+{
+  Image cbtf;
+  cbtf.readFromFile("cbtf.png");
+
+  Image de;
+  de.readFromFile("de.png");
+  de.scale(.15);
+  Image axe;
+  axe.readFromFile("axe.png");
+  axe.scale(.35);
+  Image shampoo;
+  shampoo.readFromFile("shampoo.png");
+  shampoo.scale(.2);
+
+  StickerSheet piazza(cbtf, 3);
+  piazza.addSticker(de, 100, 400);
+  piazza.addSticker(axe, 600, 350);
+  piazza.addSticker(shampoo, 350, 410);
+
+  Image cbtfNew = piazza.render();
+  cbtfNew.writeToFile("myImage.png");
+
 
   //
   // Reminder:
