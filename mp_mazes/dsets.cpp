09d9c40d0904a679d9b68424d7d7c35931a449cc
Andro Manukov <manukov2@linux-04.ews.illinois.edu>
2019-11-11 20:52:58 -0600

    mazes part1a

--- mp_mazes/dsets.cpp
+++ mp_mazes/dsets.cpp
@@ -1,1 +1,48 @@
 /* Your code here! */
+#include "dsets.h"
+
+using namespace std;
+
+void DisjointSets::addelements(int num)
+{
+  for(int i = 0; i < num; i++)
+  {
+    elems.push_back(-1);
+  }
+}
+
+int DisjointSets::find(int elem)
+{
+  if(elems[elem] < 0)
+  {
+    return elem;
+  }
+  else
+  {
+    elems[elem] = find(elems[elem]);
+    return elems[elem];
+  }
+}
+
+void DisjointSets::setunion(int a, int b)
+{
+  int first = find(a);
+  int second = find(b);
+  int size_ = elems[first] + elems[second];
+
+  if(elems[first] < elems[second])
+  {
+    elems[second] = a;
+    elems[first] = size_;
+  }
+  else
+  {
+    elems[first] = b;
+    elems[second] = size_;
+  }
+}
+
+int DisjointSets::size(int elem)
+{
+  return -1 * elems[find(elem)];
+}
--- mp_mazes/dsets.h
+++ mp_mazes/dsets.h
@@ -1,1 +1,20 @@
 /* Your code here! */
+#pragma once
+
+#include<vector>
+
+using namespace std;
+
+class DisjointSets
+{
+  public:
+    vector<int> elems;
+
+    void addelements(int num);
+    int find(int elem);
+    void setunion(int a, int b);
+    int size(int elem);
+
+  private:
+
+};
