ebe5e26e3e0fedb113cd73b960e663ddd76c275a
Manukov, Andro Sarkis <manukov2@illinois.edu>
2019-12-11 23:46:05 -0600

    mpstories submit

--- mp_stories/Graph.hpp
+++ mp_stories/Graph.hpp
@@ -5,26 +5,32 @@
 #include <string>
 #include <iostream>
 
+
+
 /**
 * @return The number of vertices in the Graph
 */
 template <class V, class E>
-unsigned int Graph<V,E>::numVertices() const {
+unsigned int Graph<V,E>::numVertices() const
+{
   // TODO: Part 2
-  return 0;
+  return vertexMap.size();
 }
 
-
+//GOOD
 /**
 * The degree of the vertex. For directed: Sum of in-degree and out-degree
 * @return Returns the degree of a given vertex.
 * @param v Given vertex to return degree.
 */
 template <class V, class E>
-unsigned int Graph<V,E>::degree(const V & v) const {
+unsigned int Graph<V,E>::degree(const V & v) const
+{
   // TODO: Part 2
-  return 0;
+  std::list<edgeListIter> out = adjList.at(v.key());
+  return out.size();
 }
+//GOOD
 
 
 /**
@@ -36,8 +42,12 @@ template <class V, class E>
 V & Graph<V,E>::insertVertex(std::string key) {
   // TODO: Part 2
   V & v = *(new V(key));
+
+  vertexMap.emplace(key, v);
+  adjList[key] = std::list<edgeListIter>();
   return v;
 }
+//GOOD
 
 
 /**
@@ -45,9 +55,17 @@ V & Graph<V,E>::insertVertex(std::string key) {
 * @param v The Vertex to remove
 */
 template <class V, class E>
-void Graph<V,E>::removeVertex(const std::string & key) {
+void Graph<V,E>::removeVertex(const std::string & key)
+{
+  std::list<std::reference_wrapper<E>> edges = incidentEdges(key);
+  for(edgeListIter iter = edges.begin(); iter != edges.end(); iter++)
+  {
+    removeEdge(iter);
+  }
+  vertexMap.erase(key);
   // TODO: Part 2
 }
+//GOOD
 
 
 /**
@@ -57,13 +75,17 @@ void Graph<V,E>::removeVertex(const std::string & key) {
 * @return The inserted Edge
 */
 template <class V, class E>
-E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
+E & Graph<V,E>::insertEdge(const V & v1, const V & v2)
+{
   // TODO: Part 2
   E & e = *(new E(v1, v2));
+  edgeList.push_front(e);
+  adjList.at(v1.key()).push_back(edgeList.begin());
+  adjList.at(v2.key()).push_back(edgeList.begin());
 
   return e;
 }
-
+//GOOD
 
 /**
 * Removes an Edge from the Graph. Consider both the undirected and directed cases.
@@ -72,10 +94,35 @@ E & Graph<V,E>::insertEdge(const V & v1, const V & v2) {
 * @param key2 The key of the destination Vertex
 */
 template <class V, class E>
-void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {  
-  // TODO: Part 2
+void Graph<V,E>::removeEdge(const std::string key1, const std::string key2)
+{
+  for(auto iter = edgeList.begin(); iter != edgeList.end(); iter++)
+  {
+    if(iter->get().directed() && iter->get().source() == vertexMap.at(key1) && iter->get().dest() == vertexMap.at(key2))
+    {
+        edgeList.erase(iter);
+        adjList.at(key1).remove(iter);
+        adjList.at(key2).remove(iter);
+        return;
+    }
+    else if(iter->get().source() == vertexMap.at(key1) && iter->get().dest() == vertexMap.at(key2))
+    {
+        edgeList.erase(iter);
+        adjList.at(key1).remove(iter);
+        adjList.at(key2).remove(iter);
+        return;
+    }
+    else if(iter->get().source() == vertexMap.at(key2) && iter->get().dest() == vertexMap.at(key1))
+    {
+        edgeList.erase(iter);
+        adjList.at(key1).remove(iter);
+        adjList.at(key2).remove(iter);
+        return;
+    }
+  }
+    // TODO: Part 2
 }
-
+//GOOD
 
 /**
 * Removes an Edge from the Graph given by the location of the given iterator into the edge list.
@@ -83,7 +130,13 @@ void Graph<V,E>::removeEdge(const std::string key1, const std::string key2) {
 * you would like to remove
 */
 template <class V, class E>
-void Graph<V,E>::removeEdge(const edgeListIter & it) {
+void Graph<V,E>::removeEdge(const edgeListIter & it)
+{
+
+  Vertex source_ = (it->get()).source().key();
+  Vertex sink_ = (it->get()).dest().key();
+
+  removeEdge(source_, sink_);
   // TODO: Part 2
 }
 
@@ -94,11 +147,18 @@ void Graph<V,E>::removeEdge(const edgeListIter & it) {
 * @param key The key of the given vertex
 * @return The list edges (by reference) that are adjacent to the given vertex
 */
-template <class V, class E>  
-const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::string key) const {
+template <class V, class E>
+const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::string key) const
+{
   // TODO: Part 2
   std::list<std::reference_wrapper<E>> edges;
+  for(edgeListIter iter : adjList.at(key))
+  {
+    edges.push_back(*iter);
+  }
   return edges;
+
+
 }
 
 
@@ -110,7 +170,28 @@ const std::list<std::reference_wrapper<E>> Graph<V,E>::incidentEdges(const std::
 * @return True if v1 is adjacent to v2, False otherwise
 */
 template <class V, class E>
-bool Graph<V,E>::isAdjacent(const std::string key1, const std::string key2) const {
-  // TODO: Part 2
+bool Graph<V,E>::isAdjacent(const std::string key1, const std::string key2) const
+{
+  std::string key_to_use = std::max(key1, key2);
+  std::list<std::reference_wrapper<E>> allEdges = incidentEdges(key_to_use);
+  for(Edge e : allEdges)
+  {
+    if(!e.directed())
+    {
+      if(e.source().key() == key1 && e.dest().key() == key2)
+      {
+        return true;
+      }
+    }
+
+    if(e.directed())
+    {
+      if(e.source().key() == key2 && e.dest().key() == key1)
+      {
+        return true;
+      }
+    }
+  }
   return false;
+  // TODO: Part 2
 }
--- mp_stories/Graph2.hpp
+++ mp_stories/Graph2.hpp
@@ -3,26 +3,77 @@
 #include <string>
 #include <list>
 
+
+using namespace std;
 /**
  * Returns an std::list of vertex keys that creates any shortest path between `start` and `end`.
  *
  * This list MUST include the key of the `start` vertex as the first vertex in the list, the key of
  * the `end` vertex as the last element in the list, and an ordered list of all vertices that must
  * be traveled along the shortest path.
- * 
+ *
  * For example, the path a -> c -> e returns a list with three elements: "a", "c", "e".
- * 
+ *
  * You should use undirected edges. Hint: There are no edge weights in the Graph.
  *
  * @param start The key for the starting vertex.
  * @param end   The key for the ending vertex.
  */
 template <class V, class E>
-std::list<std::string> Graph<V,E>::shortestPath(const std::string start, const std::string end) {
+std::list<std::string> Graph<V,E>::shortestPath(const std::string start, const std::string end)
+{
   // TODO: Part 3
   std::list<std::string> path;
 
+  queue<string> queue_;
+  unordered_map<string, string> parent;
+  bool Neighbor;
+
+  for(auto it = vertexMap.begin(); it != vertexMap.end(); it++)
+  {
+    parent[it->first] = "noEdge";
+  }
+
+  queue_.push(start);
+  parent[start] = "start";
+
+  while(!queue_.empty())
+  {
+
+    string front_ = queue_.front();
+    queue_.pop();
+
+    for(auto it = vertexMap.begin(); it != vertexMap.end(); it++)
+    {
+      Neighbor = false;
+      for(edgeListIter iter : adjList.at(front_))
+      {
+        if((*iter).get().source().key() == it->first)
+        {
+          Neighbor = true;
+        }
+        if((*iter).get().dest().key() == it->first)
+        {
+          Neighbor = true;
+        }
+      }
+
+      if(Neighbor && parent[it->first] == "noEdge")
+      {
+        queue_.push(it->first);
+        parent[it->first] = front_;
+      }
+    }
+  }
+
+  string temp = end;
+  path.push_back(end);
+
+  while(temp != start)
+  {
+    path.push_front(parent[temp]);
+    temp = parent[temp];
+  }
 
   return path;
 }
-
